3a5ba1f389e48a6ab1a73137e2c4cb4bcb2c2060
Prasanta <Prasanta.Banerjee77@gmail.com>
2020-04-25 00:50:17 +0530

    Modified: index.js by adding callback functions for PUT/PATCH/DELETE & created a file with static data.

---
+++ employeesData.js
@@ -0,0 +1,136 @@
+//Create user data.
+const employeesData = [
+    {
+        id: 656788,
+        firstName: 'Mark',
+        lastName: 'Andrews',
+        age: 24,
+        hobby: [
+            {
+                coding: ['java', 'python', 'javascript'],
+                movies: ['action', 'comedy', 'suspense'],
+                sports: "basketball"
+            }
+        ],
+        oper_sys: ['Mac', 'Windows'],
+        email: 'Mark.andrews@gmail.com',
+        website: 'http://markandrews.com'
+    },
+    {
+        id: 44551738,
+        firstName: 'Elizabeth',
+        lastName: 'Jackson',
+        age: 23
+    },
+    {
+        id: 88776,
+        firstName: 'Pamela',
+        lastName: 'Brown',
+        age: 24
+    },
+    {
+        id: 673649,
+        firstName: 'Michael',
+        lastName: 'Thomas Jr.',
+        age: 25
+    },
+    {
+        id: 673632,
+        firstName: 'George',
+        lastName: 'Stallion',
+        age: 23
+    },
+    {
+        id: 673653,
+        firstName: 'James',
+        lastName: 'Stephenson',
+        age: 25
+    },
+    {
+        id: 8946546,
+        firstName: 'Lele',
+        lastName: 'Pons',
+        profession: 'Youtuber',
+        age: 24
+    },
+    {
+        id: 654648,
+        firstName: 'Hannah',
+        lastName: 'Stocking',
+        profession: 'Youtuber'
+    },
+    {
+        id: 874987,
+        firstName: 'Anwar',
+        lastName: 'Jibawi',
+        profession: 'Youtuber'
+    },
+    {
+        id: 983472,
+        firstName: 'Adam',
+        lastName: 'Waheed',
+        profession: 'Youtuber'
+    },
+    {
+        id: 2144,
+        firstName: 'De',
+        lastName: 'Storm',
+        profession: 'Youtuber'
+    },
+    {
+        id: 2355,
+        firstName: 'King',
+        lastName: 'Bach',
+        profession: 'Youtuber'
+    },
+    {
+        id: 224455,
+        firstName: 'Rudy',
+        lastName: 'Mancuso',
+        profession: 'Youtuber'
+    },
+    {
+        id: 66887,
+        firstName: 'Twan',
+        lastName: 'Kuyper',
+        profession: 'Youtuber'
+    },
+    {
+        id: 33447,
+        firstName: 'Bhuvam',
+        lastName: 'Bam',
+        profession: 'Youtuber'
+    },
+    {
+        id: 228877,
+        firstName: 'Ashish',
+        lastName: 'Chanchalani',
+        profession: 'Youtuber'
+    },
+    {
+        id: 12456885,
+        firstName: 'Mostly',
+        lastName: 'Sane',
+        profession: 'Youtuber'
+    },
+    {
+        id: 66887423,
+        firstName: 'Amanda',
+        lastName: 'Cerny',
+        profession: 'Youtuber'
+    },
+    {
+        id: 89452212,
+        firstName: 'Inanna',
+        lastName: 'Sarkis',
+        profession: 'Youtuber'
+    },
+    {
+        id: 31394654,
+        firstName: 'Be',
+        lastName: 'YouNick',
+        profession: 'Youtuber'
+    },
+];
+
+module.exports = employeesData;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Module dependencies.
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,102 +7,77 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false })); //support encoded bodies.
 app.use(bodyParser.json());                          //support json encoded bodies.
 
-//Create user data.
-const employeesData = [
-    {
-        id : 673630,
-        firstName : 'Prasanta',
-        lastName : 'Banerjee',
-        age : 24,
-        hobby : [
-            {
-                coding : ['java', 'python', 'javascript'],
-                movies : ['action', 'comedy' , 'suspense'],
-                sports : "basketball"
-            }
-        ],
-        oper_sys : ['Mac', 'Windows']
-    },
-    {
-        id : 673631,
-        firstName : 'Neha',
-        lastName : 'Bharti',
-        age : 23
-    },
-    {
-        id : 673651,
-        firstName : 'Priyanka',
-        lastName : 'Moharana',
-        age : 24
-    },
-    {
-        id : 673649,
-        firstName : 'Shreyanshu',
-        lastName : 'Jena',
-        age : 25
-    },
-    {
-        id : 673632,
-        firstName : 'Priyanka',
-        lastName : 'Sonalia',
-        age : 23
-    },
-    {
-        id : 673653,
-        firstName : 'Bhupinder',
-        lastName : 'Singh',
-        age : 25
-    },
-];
+//Get the user data.
+let employeesData = require('./employeesData');
 
-//Create the API endpoints with callback functions.
-//Display a message.
-app.get('/api/info', function(req, res) {
-    res.send('Welcome to Employees API !!! Get access to free APIs.');
+//Create the API endpoints/routers with callback functions.
+//Display a Welcome message.
+app.get('/api/info', function (req, res) {
+    res.send('Welcome to Employees API !!! Get access to free APIs.\nThese APIs have been developed using Node & Express.');
 });
 
-//Display all Employees data.
-app.get('/api/employees', function(req, res) {
+//GET Method: Retrieve all user's data.
+app.get('/api/employees', function (req, res) {
     res.json(employeesData);
 });
 
-//Display employee data based on 'id' param.
-app.get('/api/employees/:id', function(req, res) {
+//GET Method: Retrieve user's data based on ID param.
+app.get('/api/employees/:id', function (req, res) {
     const id = req.params.id;
-    const user = employeesData.find(user => user.id == id)
-
-    if(user){
+    const user = employeesData.find(user => user.id == id);
+    if (user) {
         res.statusCode = 200
         res.json(user)
     }
     else {
         res.statusCode = 404
-        return res.json({Error: ['ID Not Found']});
+        return res.json({ Error: ['ID Not Found'] });
     }
 });
 
-const newData = {
-    id : 578895,
-    firstName : 'Abhijeet',
-    lastName : 'Agarwala',
-    age : 24
-}
-
-// POST new employees data.
+//POST Method: Create a new employee.
 app.post('/api/employees', function (req, res) {
-    const newUser = {
-        id: req.body.id,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        age: req.body.age
-    }
-
-    employeesData.push(newUser);
-    res.status(200).json(newUser);
+    employeesData.push(req.body);
+    res.status(201).json(req.body);
 });
 
-//start the node server.
+//PUT Method: Update an employee based on ID param.
+app.put('/api/employees/:id', function (req, res) {
+    const id = req.params.id;
+    const user = employeesData.find(user => user.id == id)
+    if (user) {
+        const index = employeesData.indexOf(user);
+        let keys = Object.keys(req.body);
+        keys.forEach(key => {
+            user[key] = req.body[key];
+        });
+        employeesData[index] = user;
+        res.json(employeesData[index]);
+    }
+    else {
+        res.statusCode = 404
+        return res.json({ Error: ['ID Not Found'] });
+    }
+});
+
+//DELETE: Delete an employee based on ID param.
+app.delete('/api/employees/:id', function (req, res) {
+    let id = req.params.id;
+    const user = employeesData.find(user => user.id == id)
+    if (user) {
+        const index = employeesData.indexOf(user);
+        employeesData.splice(index, 1);
+        res.json({ message: `User ${id} deleted.` });
+    }
+    else {
+        res.statusCode = 404
+        return res.json({ Error: ['ID Not Found'] });
+    }
+})
+
+//Start the Node server.
+const hostname = 'localhost';
 const PORT = 7777;
-app.listen(PORT, function() {
-    console.log('Your server is up & running at localhost:' + PORT + '. Please hit the APIs.');
+app.listen(PORT, function () {
+    console.log('Your server is up & running at http://' + hostname + ':' + PORT + '.');
 });
