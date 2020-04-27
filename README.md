81b0dac64f7d17d7e5bcc3b86be17b1d1d86a0e6
Prasanta <Prasanta.Banerjee77@gmail.com>
2020-04-28 00:22:23 +0530

    Minor code cleanup, deleting unused imports & dependencies & adding comments.

---
+++ README.md
@@ -0,0 +1,21 @@
+# API-Development_Express & NodeJS
+
+This repository will serve as a codebase for the Free APIs developed using ExpressJS & NodeJS.
+Now, users can create custom APIs with fake data and test them easily with different HTTP Verbs (GET/POST/PUT/DELETE). 
+The data is retrieved from a local static file and new data POSTed to the server is saved in-memory. 
+
+Tools & Libraries:
+====================
+  1. NodeJS : JavaScript runtime environment – lets you implement your application back-end in JavaScript.
+  2. ExpressJS : a feature-rich javascript based framework running on top of Node.js.
+  3. VScode : Integrated Development Environment.
+  4. JavaScript: Programming Language.
+
+Developed & Maintained by:
+============================
+  Prasanta Banerjee.
+  
+  QA Automation Engineer.
+  
+  E-Mail: Prasanta.Banerjee77@gmail.com
+  
--- server.js
+++ server.js
@@ -1,19 +1,39 @@
+'use strict';
+
 //Module dependencies.
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth')
+ 
+//Here we are configuring the credentials to be used for Basic Authentication.
+app.use(basicAuth({
+    users: { 
+        'Prasanta': 'Banerjee',
+        'admin': 'p@ssword123'
+    },
+    challenge: true,                                //browser-popup requesting for username & password.
+    unauthorizedResponse: getUnauthorizedResponse   //function called if authentication failed.
+}));
+
+//Function called when Basic Authentication failed.
+function getUnauthorizedResponse(req) {
+    return req.auth
+        ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected.')
+        : 'No Credentials Provided.'
+};
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false })); //support encoded bodies.
 app.use(bodyParser.json());                          //support json encoded bodies.
 
-//Get the user data.
+//----------- Get the JSON Data ------------//
 let employeesData = require('./employeesData');
 
 //Create the API endpoints/routers with callback functions.
-//Display a Welcome message.
-app.get('/api/info', function (req, res) {
-    res.send('Welcome to Employees API !!! Get access to free APIs.\nThese APIs have been developed using Node & Express.');
+//---- Display a welcome message ----//
+app.get('/api/', function (req, res) {
+    res.send('Welcome to Employees API !!! Get access to free APIs. These APIs have been developed using Node & Express.');
 });
 
 //GET Method: Retrieve all user's data.
@@ -21,7 +41,7 @@ app.get('/api/employees', function (req, res) {
     res.json(employeesData);
 });
 
-//GET Method: Retrieve user's data based on ID param.
+//GET Method: Retrieve user's data based on 'ID' param.
 app.get('/api/employees/:id', function (req, res) {
     const id = req.params.id;
     const user = employeesData.find(user => user.id == id);
@@ -41,7 +61,7 @@ app.post('/api/employees', function (req, res) {
     res.status(201).json(req.body);
 });
 
-//PUT Method: Update an employee based on ID param.
+//PUT Method: Update an employee based on 'ID' param.
 app.put('/api/employees/:id', function (req, res) {
     const id = req.params.id;
     const user = employeesData.find(user => user.id == id)
@@ -60,24 +80,34 @@ app.put('/api/employees/:id', function (req, res) {
     }
 });
 
-//DELETE: Delete an employee based on ID param.
+//DELETE: Delete an employee based on 'ID' param.
 app.delete('/api/employees/:id', function (req, res) {
     let id = req.params.id;
     const user = employeesData.find(user => user.id == id)
+    /*
+    //get headers.
+    console.log(req.headers)
+    //set headers.
+    res.set('Content-Type', 'text/html')
+    //validate the new value set.
+    console.log(req.headers)
+    */
     if (user) {
         const index = employeesData.indexOf(user);
         employeesData.splice(index, 1);
-        res.json({ message: `User ${id} deleted.` });
+        res.json({ message: `User ${id} deleted.`} );
     }
     else {
         res.statusCode = 404
         return res.json({ Error: ['ID Not Found'] });
     }
-})
+});
 
 //Start the Node server.
 const hostname = 'localhost';
-const PORT = 7777;
+//Use port 7777 unless the port number is configured in process.env.PORT.
+//Since process.env.PORT is UNDEFINED, hence the server will listen to 7777.
+const PORT = process.env.PORT  || 7777;
 app.listen(PORT, function () {
-    console.log('Your server is up & running at http://' + hostname + ':' + PORT + '.');
+    console.log('Your server is up & running at http://' + hostname + ':' + PORT);
 });
